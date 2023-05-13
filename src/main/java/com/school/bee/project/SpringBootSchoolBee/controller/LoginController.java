8a7b270489ebf9cdbeedf37d8f4f553b636d839f
Shubham.Rai <Shubham.Rai@ukg.com>
2023-05-13 19:59:43 +0530

    LoginController

---
+++ src/main/java/com/school/bee/project/SpringBootSchoolBee/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.school.bee.project.SpringBootSchoolBee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	@RequestMapping(value="/login" , method = RequestMethod.GET)
+	public String login() {
+		return "You are Successfully LoggedIn";
+	}
+
+}
